cd5f7716d24444eb83132d08bf2a3b230705e3e8
dobermann112 <ruffy112@icloud.com>
2026-06-04 14:51:34 +0900

    投稿いいね削除用のAPIを実装

--- src/app/api/posts/[id]/likes/route.ts
+++ src/app/api/posts/[id]/likes/route.ts
@@ -68,3 +68,60 @@ export async function POST(request: Request, { params }: { params: Promise<{ id:
     )
   }
 }
+
+export async function DELETE(request: Request, { params }: { params: Promise<{ id: string }> }) {
+  try {
+    const { id } = await params
+    const postId = Number(id)
+
+    if (Number.isNaN(postId)) {
+      return NextResponse.json(
+        { error: "Invalid post Id" },
+        { status: 400 }
+      )
+    }
+
+    const session = await getServerSession(authOptions)
+
+    if (!session?.user?.id) {
+      return NextResponse.json(
+        { error: "Unauthorized" },
+        { status: 401 }
+      )
+    }
+
+    const existingLike = await prisma.like.findUnique({
+      where: {
+        userId_postId: {
+          userId: Number(session.user.id),
+          postId,
+        },
+      },
+    })
+
+    if (!existingLike) {
+      return NextResponse.json(
+        { error: "Like not found" },
+        { status: 404 }
+      )
+    }
+
+    await prisma.like.delete({
+      where: {
+        userId_postId: {
+          userId: Number(session.user.id),
+          postId,
+        },
+      },
+    })
+
+    return NextResponse.json({ id: existingLike.id }, { status: 200 })
+  } catch (error) {
+    console.error("Error", error)
+
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    )
+  }
+}
