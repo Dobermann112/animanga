568d89911c0f1925e1505178183dab9b30063e40
dobermann112 <ruffy112@icloud.com>
2026-07-09 16:13:59 +0900

    レビューターゲットのUIを変更

--- src/components/ReviewTargetTag.tsx
+++ src/components/ReviewTargetTag.tsx
@@ -7,24 +7,24 @@ type Props = {
 export default function ReviewTargetTag({ reviewTarget }: Props) {
   if (reviewTarget === "ANIME") {
     return (
-      <span className="text-xs bg-orange-500 text-white px-2 py-1 rounded mr-2">
+      <span className="text-xs bg-orange-500 text-white px-2 py-1 rounded-md mr-2">
         アニメ
       </span>
     )
   }
   if (reviewTarget === "MANGA") {
     return (
-      <span className="text-xs bg-orange-500 text-white px-2 py-1 rounded mr-2">
+      <span className="text-xs bg-orange-500 text-white px-2 py-1 rounded-md mr-2">
         漫画
       </span>
     )
   }
   return (
     <>
-      <span className="text-xs bg-orange-500 text-white px-2 py-1 rounded mr-2">
+      <span className="text-xs bg-orange-500 text-white px-2 py-1 rounded-md mr-2">
         漫画
       </span>
-      <span className="text-xs bg-orange-500 text-white px-2 py-1 rounded">
+      <span className="text-xs bg-orange-500 text-white px-2 py-1 rounded-md">
         アニメ
       </span>
     </>
